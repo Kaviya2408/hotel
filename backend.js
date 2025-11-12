4846bd8a8275cbe3f6b110336c37b607cb312f09
Kaviya2408 <kit27.csbs29@gmail.com>
2025-11-12 13:32:42 +0530

    implemented backend for signup

---
+++ backend.js
@@ -0,0 +1,33 @@
+const express=require('express')
+const bodyParser=require('body-parser')
+const mysql=require('mysql')
+const path=require('path')
+
+const app=express()
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(__dirname))
+
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'2624',
+    database:'hotelusers' //change to your database name
+})
+
+db.connect(err=>{if(err)throw err;console.log("MySQL Connected")})
+
+app.post('/signup',(req,res)=>{
+    const{name,email,phone,password,address}=req.body
+    const sql="INSERT INTO users(name,email,phone,password,address) VALUES(?,?,?,?,?)"
+    db.query(sql,[name,email,phone,password,address],err=>{
+	if(err){
+		console.log(err)
+		return res.send("Error: "+err.message)
+	}
+	res.redirect('index.html')
+    })
+})
+
+
+
+app.listen(3000,()=>console.log("Server running at http://localhost:3000"))
